b7edca4ef6aa638b63739f2e55889631a3b82330
Tim Wegmeyer <wegmeyert@gmail.com>
2025-01-12 15:16:24 +0100

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,13 @@ import './Header.css';
 function Header() {
     return (
       <header className="header">
+        <img class="headerLogo" src="/Blaumann-Logo.png" alt="Blaumann Logo"></img>
         <h1>Blaumann</h1>
         <nav>
           <ul>
             <li><a href="#home">Auftrag Einstellen</a></li>
             <li><a href="#about">Über uns</a></li>
+            <li><a href="#Blog">Blog</a></li>
             <li><a href="#contact">Als Handwerker Anmelden</a></li>
           </ul>
         </nav>
